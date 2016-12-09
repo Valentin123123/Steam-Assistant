8c6d4c07ca3731924db03bb5425ea10d49575807
Valentin123123 <valentin.schultheis@gmx.at>
2016-12-09 19:07:26 +0100

    Add files via upload

--- Steam_Assistant/Steam_Assistant/About.cs
+++ Steam_Assistant/Steam_Assistant/About.cs
@@ -8,6 +8,13 @@ namespace Steam_Assistant
 {
     class About
     {
+        int _version;
+        public int Version {
+            get { return _version; }
+            set { _version = Version; }
+        }
+
+        string Authors;
         public void Info()
         {
             Console.WriteLine("This tool makes use of Valve's Steam API to provide helpful information" + "\n" + "about games available on the platform and much more!");
