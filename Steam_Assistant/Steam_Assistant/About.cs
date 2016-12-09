f06a93fb47f6516f240d25ac5b131d65125e32ae
Valentin123123 <valentin.schultheis@gmx.at>
2016-12-09 18:48:14 +0100

    Add files via upload

---
+++ Steam_Assistant/Steam_Assistant/About.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam_Assistant
+{
+    class About
+    {
+        public void Info()
+        {
+            Console.WriteLine("This tool makes use of Valve's Steam API to provide helpful information" + "\n" + "about games available on the platform and much more!");
+        }
+        
+    }
+}
---
+++ Steam_Assistant/Steam_Assistant/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam_Assistant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            About about = new About();
+            about.Info();
+
+            Console.ReadKey();
+        }
+    }
+}
